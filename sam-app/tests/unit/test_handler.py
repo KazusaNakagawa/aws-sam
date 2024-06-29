5a150f88c80a2b8c42993d178bd34d6fe97cd141
KazusaNakagawa <kazusanakagawa@gmail.com>
2024-06-29 19:02:34 +0900

    no test

--- sam-app/tests/unit/test_handler.py
+++ sam-app/tests/unit/test_handler.py
@@ -5,20 +5,5 @@ import pytest
 from handler import s3copy
 
 class TestHandler:
-
-      def test_handler(self):
-         event = {
-               'Records': [
-                  {
-                     's3': {
-                           'bucket': {
-                              'name': 'sourcebucket'
-                           },
-                           'object': {
-                              'key': 'HappyFace.jpg'
-                           }
-                     }
-                  }
-               ]
-         }
-         assert s3copy(event, None) == 'Hello from Lambda!'
+  # TODO: Add more tests
+  pass
