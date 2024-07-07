0afc4110538d2fe4d5b76bc57ce282fa8ca833d9
KazusaNakagawa <kazusanakagawa@gmail.com>
2024-07-07 11:02:57 +0900

    wip: 一旦実現したい実装はできた。改変が必要な点は、TODOとした

--- sam-app/shell/setup_s3_trigger.py
+++ sam-app/shell/setup_s3_trigger.py
@@ -1,7 +1,8 @@
 import boto3
 import json
 import sys
-import time
+
+COMMON_BUCKET_NAME = "s3-copy-input-bucket-common"
 
 
 def get_lambda_arn(lambda_client, function_name):
@@ -43,26 +44,46 @@ def update_s3_notification(s3_client, bucket_name, new_config):
     s3_client.put_bucket_notification_configuration(Bucket=bucket_name, NotificationConfiguration=current_config)
 
 
-def clear_s3_notification(s3_client, bucket_name):
-    s3_client.put_bucket_notification_configuration(Bucket=bucket_name, NotificationConfiguration={})
+def trigger_s3_event(s3_client, bucket_name, lambda_arn, s3_prefix, s3_suffix):
+    new_notification_config = {
+        "LambdaFunctionArn": lambda_arn,
+        "Events": ["s3:ObjectCreated:*"],
+        "Filter": {"Key": {"FilterRules": [{"Name": "prefix", "Value": s3_prefix}]}},
+    }
+
+    if s3_suffix:
+        new_notification_config["Filter"]["Key"]["FilterRules"].append({"Name": "suffix", "Value": s3_suffix})
+
+    print(f"New Notification Configuration: {json.dumps(new_notification_config, indent=2)}")
+
+    current_notification_config = s3_client.get_bucket_notification_configuration(Bucket=bucket_name)
+    # Remove ResponseMetadata from current configuration
+    current_notification_config.pop("ResponseMetadata", None)
+
+    if "LambdaFunctionConfigurations" not in current_notification_config:
+        current_notification_config["LambdaFunctionConfigurations"] = []
+
+    current_notification_config["LambdaFunctionConfigurations"].append(new_notification_config)
+
+    return current_notification_config
 
 
-def main(env, input_bucket_name, profile):
-    session = boto3.Session(profile_name=profile)
-    lambda_client = session.client("lambda")
-    s3_client = session.client("s3")
-    sts_client = session.client("sts")
+def create_unique_statement_id(lambda_function_name, lambda_client, bucket_name, account_id):
+    """既存のステートメントIDを確認し、存在する場合はそれを削除してから同盟のステートメントIDを作成する
+    そうすることで、同じステートメントIDで複数のLambda関数に対して権限を付与することを防ぐ
+    """
+    statement_id = f"{lambda_function_name}-s3-invoke-{bucket_name}"
+    # Remove existing permission if exists
+    remove_existing_permission(lambda_client, lambda_function_name, statement_id)
+    # Add new permissionzxxz
+    add_lambda_permission(lambda_client, lambda_function_name, bucket_name, statement_id, account_id)
 
-    account_id = sts_client.get_caller_identity()["Account"]
 
-    lambda_events = [
-        ("s3-copy-lambda-{}".format(env), "{}-{}".format(input_bucket_name, env), "input/", ".json"),
-        ("s3-copy-lambda2-{}".format(env), "{}-{}".format(input_bucket_name, env), "/prefix/", ".tsv.gz"),
-        ("s3-copy-lambda2-{}".format(env), "{}-{}".format(input_bucket_name, env), "/prefix2/", "*.tsv.gz"),
-    ]
+def create_s3_trigger(env, s3_client, lambda_client, account_id, lambda_events, input_bucket_name):
 
     # Clear existing notification configuration to avoid conflicts
-    clear_s3_notification(s3_client, "{}-{}".format(input_bucket_name, env))
+    s3_client.put_bucket_notification_configuration(Bucket=f"{input_bucket_name}-{env}", NotificationConfiguration={})
+    s3_client.put_bucket_notification_configuration(Bucket=COMMON_BUCKET_NAME, NotificationConfiguration={})
 
     for lambda_function_name, bucket_name, s3_prefix, s3_suffix in lambda_events:
         print(f"Lambda Function Name: {lambda_function_name}")
@@ -73,44 +94,36 @@ def main(env, input_bucket_name, profile):
         lambda_arn = get_lambda_arn(lambda_client, lambda_function_name)
         print(f"Lambda Function ARN: {lambda_arn}")
 
-        statement_id = f"{lambda_function_name}-s3-invoke"
+        create_unique_statement_id(lambda_function_name, lambda_client, bucket_name, account_id)
 
-        # Remove existing permission if exists
-        remove_existing_permission(lambda_client, lambda_function_name, statement_id)
-
-        # Add new permission
-        add_lambda_permission(lambda_client, lambda_function_name, bucket_name, statement_id, account_id)
-
-        new_notification_config = {
-            "LambdaFunctionArn": lambda_arn,
-            "Events": ["s3:ObjectCreated:*"],
-            "Filter": {"Key": {"FilterRules": [{"Name": "prefix", "Value": s3_prefix}]}},
-        }
-
-        if s3_suffix:
-            new_notification_config["Filter"]["Key"]["FilterRules"].append({"Name": "suffix", "Value": s3_suffix})
-
-        print(f"New Notification Configuration: {json.dumps(new_notification_config, indent=2)}")
-
-        current_notification_config = s3_client.get_bucket_notification_configuration(Bucket=bucket_name)
-
-        # Remove ResponseMetadata from current configuration
-        current_notification_config.pop("ResponseMetadata", None)
-
-        if "LambdaFunctionConfigurations" not in current_notification_config:
-            current_notification_config["LambdaFunctionConfigurations"] = []
-
-        current_notification_config["LambdaFunctionConfigurations"].append(new_notification_config)
+        current_notification_config = trigger_s3_event(s3_client, bucket_name, lambda_arn, s3_prefix, s3_suffix)
 
         updated_notification_config = json.dumps(current_notification_config)
-
         print(f"Updated Notification Configuration: {updated_notification_config}")
 
         s3_client.put_bucket_notification_configuration(
             Bucket=bucket_name, NotificationConfiguration=current_notification_config
         )
-
         print(f"Successfully configured S3 trigger for Lambda function {lambda_function_name}.")
+
+
+def main(env, input_bucket_name, profile):
+    session = boto3.Session(profile_name=profile)
+    lambda_client = session.client("lambda")
+    s3_client = session.client("s3")
+    sts_client = session.client("sts")
+
+    account_id = sts_client.get_caller_identity()["Account"]
+
+    # TODO: ここは設定ファイルから読み込むようにすると処理を切り分けやすくなる
+    lambda_events = [
+        (f"s3-copy-lambda-{env}", f"{input_bucket_name}-{env}", "input/", ".json"),
+        (f"s3-copy-lambda2-{env}", f"{input_bucket_name}-{env}", "prefix/", ".tsv.gz"),
+        (f"s3-copy-lambda-{env}", COMMON_BUCKET_NAME, "input/", ".tsv.gz"),
+        (f"s3-copy-lambda2-{env}", COMMON_BUCKET_NAME, "input2/", "*.tsv.gz"),
+    ]
+
+    create_s3_trigger(env, s3_client, lambda_client, account_id, lambda_events, input_bucket_name)
 
 
 if __name__ == "__main__":
